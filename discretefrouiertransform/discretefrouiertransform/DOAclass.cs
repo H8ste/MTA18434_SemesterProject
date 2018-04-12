7d3bcba386e69019e64938a931b52dc43e3e85af
Unknown <mathias.s.lynge@gmail.com>
2018-04-12 20:34:12 +0200

    Added correlation function
    This only works in the time domain

---
+++ discretefrouiertransform/discretefrouiertransform/DOAclass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace discretefrouiertransform
+{
+    public class DOAclass
+    {
+        double meanx, meany, denom, sx, sy, sxy;
+        int i, j;
+ 
+        public DOAclass()
+        {
+            
+        }
+        public double CrossCorrelation(double[] x, double[] y, int Samples, int maxDelay)
+        {
+            meanx = 0;
+            meany = 0;
+            double r = 0;
+
+           
+           
+            double[] tempx = x;
+            double[] tempy = y;
+
+            //Calculating the mean
+            for (int i = 0; i < Samples; i++)
+            {
+                meanx += x[i];
+                meany += y[i];
+            }
+            meanx /= Samples;
+            meany /= Samples;
+
+
+            //Denominator
+            sx = 0;
+            sy = 0;
+            for (i = 0; i < Samples; i++)
+            {
+                sx += (tempx[i] - meanx) * (x[i] - meanx);
+                sy += (tempy[i] - meany) * (y[i] - meany);
+            }
+            denom = Math.Sqrt(sx * sy);
+
+            //Calculate the correlation series
+            for (int delay = -maxDelay; delay < maxDelay; delay++)
+            {
+                sxy = 0;
+                for (i = 0; i < Samples; i++)
+                {
+                    j = i + delay;
+                    if (j < 0 || j >= Samples)
+                        continue;
+                    else
+                        sxy += (x[i] - meanx) * (y[j] - meany);
+                    /* Or should it be (?)
+                    if (j < 0 || j >= n)
+                       sxy += (x[i] - mx) * (-my);
+                    else
+                       sxy += (x[i] - mx) * (y[j] - my);
+                    */
+                }
+                r = sxy / denom;
+
+            }
+            return r;
+        }
+    }
+}

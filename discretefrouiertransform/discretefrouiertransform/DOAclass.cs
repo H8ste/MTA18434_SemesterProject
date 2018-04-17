b82c19552c6c96206711cda22306a80070666879
Unknown <mathias.s.lynge@gmail.com>
2018-04-17 13:10:01 +0200

    Clean-up in code

--- discretefrouiertransform/discretefrouiertransform/DOAclass.cs
+++ discretefrouiertransform/discretefrouiertransform/DOAclass.cs
@@ -4,36 +4,32 @@ namespace discretefrouiertransform
 {
     public class DOAclass
     {
-        double meanx, meany, denom, sx, sy, sxy;
-        int i, j;
+        double meanx, meany, denom, sx, sy, sxy, max, r;
+        int i, j, BestDelay;
 
-        public int CrossCorrelation(double[] x, double[] y, int Samples, int maxDelay)
+        public  int CrossCorrelation(double[] x, double[] y, int Samples, int maxDelay)
         {
-            meanx = 0;
-            meany = 0;
-            double r;
-            double max = 0;
-            int BestDelay = 0;
+            meanx = 0; meany = 0; max = 0; r = 0; BestDelay = 0;
             double[] tempx = x;
             double[] tempy = y;
 
-            //Calculating the mean
+            //Calculating the mean of both signals
             for (int i = 0; i < Samples; i++)
             {
-                meanx += x[i];
-                meany += y[i];
+                meanx += tempx[i];
+                meany += tempy[i];
             }
             meanx /= Samples;
             meany /= Samples;
 
 
-            //Denominator
+            //Denominator for normalization
             sx = 0;
             sy = 0;
             for (i = 0; i < Samples; i++)
             {
-                sx += (tempx[i] - meanx) * (x[i] - meanx);
-                sy += (tempy[i] - meany) * (y[i] - meany);
+                sx += (tempx[i] - meanx) * (tempx[i] - meanx);
+                sy += (tempy[i] - meany) * (tempy[i] - meany);
             }
             denom = Math.Sqrt(sx * sy);
 
@@ -44,23 +40,23 @@ namespace discretefrouiertransform
                 for (i = 0; i < Samples; i++)
                 {
                     j = i + delay;
-                    /*
+                    //Normal Crosscorrelation
                     if (j < 0 || j >= Samples)
                         continue;
                     else
                         sxy += (x[i] - meanx) * (y[j] - meany);
-                        */
-
+                    /*
+                    //Circular Crosscorrelation
                     if (j < 0 || j >= Samples)
                         sxy += (x[i] - meanx) * (-meany);
                     else
                         sxy += (x[i] - meanx) * (y[j] - meany);
-                    //Console.Write(sxy + " , ");
+                    */
                 }
 
                 r = sxy / denom;
-                Console.WriteLine("r: " + r);
-                Console.WriteLine("delay: " + delay);
+                //Console.WriteLine("r: " + r);
+                //Console.WriteLine("delay: " + delay);
                 if (r > max)
                 {
                     max = r;
@@ -69,70 +65,5 @@ namespace discretefrouiertransform
             }
             return BestDelay;
         }
-
-        public int CrossCorrelationFFS(double[] x, double[] y, int Samples, int maxDelay)
-        {
-            meanx = 0;
-            meany = 0;
-            double r;
-            double max = 0;
-            int BestDelay = 0;
-            double[] tempx = x;
-            double[] tempy = y;
-
-            //Calculating the mean
-            for (int i = 0; i < Samples; i++)
-            {
-                meanx += x[i];
-                meany += y[i];
-            }
-            meanx /= Samples;
-            meany /= Samples;
-
-
-            //Denominator
-            sx = 0;
-            sy = 0;
-            for (i = 0; i < Samples; i++)
-            {
-                sx += (tempx[i] - meanx) * (x[i] - meanx);
-                sy += (tempy[i] - meany) * (y[i] - meany);
-            }
-            denom = Math.Sqrt(sx * sy);
-
-            //Calculate the correlation series
-            for (int delay = -maxDelay; delay < maxDelay; delay++)
-            {
-                sxy = 0;
-                for (i = 0; i < Samples; i++)
-                {
-                    j = i + delay;
-                    /*
-                    if (j < 0 || j >= Samples)
-                        continue;
-                    else
-                        sxy += (x[i] - meanx) * (y[j] - meany);
-                        */
-
-                    if (j < 0 || j >= Samples)
-                        sxy += (x[i] - meanx) * (-meany);
-                    else
-                        sxy += (x[i] - meanx) * (y[j] - meany);
-                    //Console.Write(sxy + " , ");
-                }
-
-                r = sxy / denom;
-                Console.WriteLine("r: " + r);
-                Console.WriteLine("delay: " + delay);
-                if (r > max)
-                {
-                    max = r;
-                    BestDelay = delay;
-                }
-            }
-            return BestDelay;
-        }
-
-
     }
 }
--- discretefrouiertransform/discretefrouiertransform/Program.cs
+++ discretefrouiertransform/discretefrouiertransform/Program.cs
@@ -6,25 +6,19 @@ namespace discretefrouiertransform
 {
     class Program
     {
-        //static short[] audioStored_temp1;
-        //static short[] audioStored_temp2;
-
-        //static short[] audioStored_temp3;
-
-        //private static String filepath = "C:/Users/Nickl/Documents/Visual Studio 2017/Projects/beamformingphasor/discretefrouiertransform/discretefrouiertransform/testingrec.m4a";
-        //private static String filepath = AppDomain.CurrentDomain.BaseDirectory + "testing.wav";
+       
 
         static void Main(string[] args)
         {
-
+            DOAclass DOA = new DOAclass();
             WaveFileObject fileObject1 = new WaveFileObject("/Users/Lynge/Desktop/Audio 1HejSiriStoej.wav");
             WaveFileObject fileObject2 = new WaveFileObject("/Users/Lynge/Desktop/Audio 2HejSiriStoej.wav");
+
             int Samples = fileObject1.soundData.Count;
-            Console.WriteLine(Samples);
+
+
             double[] buffer1 = new double[Samples];
             double[] buffer2 = new double[Samples];
-
-            
 
             for (int i = 0; i < Samples; i++)
             {
@@ -32,10 +26,10 @@ namespace discretefrouiertransform
                 buffer2[i] = fileObject2.soundData[i];
             }
 
-            DOAclass dO = new DOAclass();
-            int CalDelay = dO.CrossCorrelation(buffer1,buffer2,buffer1.Length,200);
-            Console.WriteLine("Best delay " + CalDelay);
+           
+            var CalDelay = DOA.CrossCorrelation(buffer1,buffer2,buffer1.Length,200);
 
+            Console.WriteLine("Best delay " + CalDelay);
         }
     }
 }

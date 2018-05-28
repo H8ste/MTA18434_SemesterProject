7716d4a6ead2926d67acf1f37c83c2722bd241ba
Heinevolder <basdreng1902@gmail.com>
2018-05-28 15:14:22 +0200

    Update Program.cs

--- discretefrouiertransform/discretefrouiertransform/Program.cs
+++ discretefrouiertransform/discretefrouiertransform/Program.cs
@@ -14,21 +14,12 @@ namespace discretefrouiertransform
         static void Main(string[] args)
         {
             
-        
-
-            //ExampleUsePlanDirectly();
             Console.WriteLine("Enter to start recording");
             Console.WriteLine();
         
             filenameFILE = Console.ReadLine();
             AudioBuffers inputAudio = new AudioBuffers(8000, 2);
             Console.Write("Number of samples: ");
-            //short[] file1 = inputAudio.readAndWrite("C:/Users/Heine/Desktop/rec/beamformertest/beamformeronly/" + "0_800hz_beamformer.wav");
-            //short[] file2 = inputAudio.readAndWrite("C:/Users/Heine/Desktop/rec/beamformertest/beamformeronly/" + "0_1600hz_beamformer.wav");
-
-            //inputAudio.BeamAndWrite(file1, file2);
-
-            // C:\Users\Heine\Desktop\rec\beamformertest\beamformeronly
 
             inputAudio.WaveInVar.StartRecording();
             Console.WriteLine("Recording " + filenameFILE);
@@ -38,12 +29,5 @@ namespace discretefrouiertransform
 
             }
         }
-       
-
-       
-        //FFT radix-2 algorithm
-        //N chose is: N = 2^B
-
-        //      f = (k/N) * f.s
     }
 }

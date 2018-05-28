7df338542869244250e66b8d03e3d8f20efc983c
Heinevolder <basdreng1902@gmail.com>
2018-05-28 15:19:10 +0200

    Update AudioBuffers.cs

--- discretefrouiertransform/discretefrouiertransform/AudioBuffers.cs
+++ discretefrouiertransform/discretefrouiertransform/AudioBuffers.cs
@@ -143,15 +143,12 @@ namespace discretefrouiertransform
 
         public double[] timeDelaySignal(short[] signalInput, double s)
         {
-            //Console.WriteLine("Runs");
 
             double[] input = new double[signalInput.Length];
             for (int j = 0; j < signalInput.Length; j++)
             {
                 input[j] = (double)signalInput[j];
             }
-
-            //Console.WriteLine("Runs");
 
             Complex[] output = new Complex[input.GetLength(input.Rank - 1) / 2 + 1];
             double[] inOut = new double[input.Length];
@@ -160,9 +157,6 @@ namespace discretefrouiertransform
             using (var pinOut = new PinnedArray<Complex>(output))
             using (var in1Out = new PinnedArray<double>(inOut))
             {
-
-                //Console.WriteLine("fak");
-
                 DFT.FFT(pinIn, pinOut);
                 for (int j = 0; j < pinOut.Length; j++)
                 {
@@ -175,23 +169,14 @@ namespace discretefrouiertransform
                 {
                     inOut[j] = inOut[j] / input.Length;
                 }
-
-                //Console.WriteLine("pak");
-
                 return inOut;
             }
         }
 
         public void waveIn_DataAvailable(object sender, WaveInEventArgs e)
         {
-            //Console.WriteLine(e.BytesRecorded + " samples recieved");
 
-            //Console.WriteLine(e.BytesRecorded/2);
-            //RUN EVERY SAMPLING RATE / 10 * CHANNELS
-
-            //originalSignal = new double[e.BytesRecorded / 2];
             int k = 0;
-            //bool first = true;
             for (int index = 0; index < e.BytesRecorded; index += 2)
             {
                 short sample = (short)((e.Buffer[index + 1] << 8) | e.Buffer[index + 0]);
@@ -202,18 +187,8 @@ namespace discretefrouiertransform
 
 
             splitInput(mics[0]);
-
-            //printBuffer(mics[0], "ORIGINAL");
-            ////printBuffer(mics[0], "ORIGINAL SIGNAL - FIRSTMIC");
-            ////printBuffer(mics[1], "ORIGINAL SIGNAL - SECONDMIC");
-
-            ////Console.Clear();
-
-            //printBuffer(Buffers, "SEPERATED INPUT SIGNAL");
             for (int j = 0; j < Buffers.Count; j++)
             {
-                //    //Console.WriteLine("Buffer: " + Buffers.Count);
-                //    //Console.WriteLine("runs " + j);
                 double AngleList = 50;
                 double LenghtMic = 0.075;
                 double reset = (1) * LenghtMic * Math.Cos(90 * Math.PI / 180) / 343;
@@ -221,13 +196,9 @@ namespace discretefrouiertransform
                 ShiftedBuffers[j] = timeDelaySignal(Buffers[j], shiftinseconds);
             }
 
-            ////Console.WriteLine("Stuck");
-
-            //printBuffer(ShiftedBuffers, "SHIFTED SEPERATED INPUT SIGNAL");
             for (int j = 0; j < OutputSignal.Length; j++)
             {
-                //double hann = 0.5 * (1 - Math.Cos(2 * Math.PI * j / (ShiftedBuffers[0].Length - 1)));
-
+   
                 if (firsttime != true && j < ShiftedBuffers[0].Length / 2)
                 {
                     double hann1 = 0.5 * (1 - Math.Cos(2 * Math.PI * (j) / (ShiftedBuffers[0].Length - 1)));
@@ -236,7 +207,6 @@ namespace discretefrouiertransform
                 }
                 else if (firsttime == true && j < ShiftedBuffers[0].Length / 2)
                 {
-                    //double hann = 0.5 * (1 - Math.Cos(2 * Math.PI * j / (ShiftedBuffers[0].Length - 1)));
                     OutputSignal[j] = ShiftedBuffers[0][j];
                 }
                 else if (j >= ShiftedBuffers[0].Length / 2 && j < ShiftedBuffers[0].Length)
@@ -264,62 +234,29 @@ namespace discretefrouiertransform
                 }
             }
 
-
-
             i++;
             firsttime = false;
-
-            //printBuffer(mics[0], "ORIGINAL SIGNAL");
-            //printBuffer(OutputSignal, "FINALSHIFTED SIGNAL");
-
             double[] beamformedSignal = new double[mics[0].Length];
             for (int j = 0; j < beamformedSignal.Length; j++)
                 beamformedSignal[j] = (mics[1][j] + OutputSignal[j])/2;
-
-            //printBuffer(beamformedSignal, "BEAMFORMED SIGNAL");
-            //for (int j = 0; j < mics[0].Length; j++)
-            //{
-            //    combinedOutput.Add(beamformedSignal[j]);
-            //}
-            //List<byte> byteArr = new List<byte>();
-
             for (int j = 0; j < beamformedSignal.Length; j++)
             {
                 byte[] tempByteArr = BitConverter.GetBytes((short)beamformedSignal[j]);
-                //Console.WriteLine("Starting to Append file");
                 writer.Write(tempByteArr, 0, tempByteArr.Length);
-                //Console.WriteLine("File Appended");
                 duration++;
-                //for (int l = 0; l < 2; l++)
-                //{
-                //    byteArr.Add(tempByteArr[l]);
-                //}
-                
             }
             
             for (int j = 0; j < OutputSignal.Length; j++)
             {
                 byte[] tempByteArr = BitConverter.GetBytes((short)mics[0][j]);
-                //Console.WriteLine("Starting to Append file");
                 writerOriginal1.Write(tempByteArr, 0, tempByteArr.Length);
-                //Console.WriteLine("File Appended");
-                //for (int l = 0; l < 2; l++)
-                //{
-                //    byteArr.Add(tempByteArr[l]);
-                //}
             }
             
 
             for (int j = 0; j < OutputSignal.Length; j++)
             {
                 byte[] tempByteArr = BitConverter.GetBytes((short)mics[1][j]);
-                //Console.WriteLine("Starting to Append file");
                 writerOriginal2.Write(tempByteArr, 0, tempByteArr.Length);
-                //Console.WriteLine("File Appended");
-                //for (int l = 0; l < 2; l++)
-                //{
-                //    byteArr.Add(tempByteArr[l]);
-                //}
             }
             
 
@@ -328,23 +265,10 @@ namespace discretefrouiertransform
             {
                 double currentNumber = (mics[0][j] + mics[1][j])/2.0;
                 byte[] tempByteArr = BitConverter.GetBytes((short)Math.Round(currentNumber));
-                //Console.WriteLine("Starting to Append file");
                 combinedWriter.Write(tempByteArr, 0, tempByteArr.Length);
-                //Console.WriteLine("File Appended");
-                //for (int l = 0; l < 2; l++)
-                //{
-                //    byteArr.Add(tempByteArr[l]);
-                //}
             }
             
 
-
-
-
-
-
-
-            //WaveFileObject.WriteWaveFile(byteArr.ToArray(), "C:/Users/Nickl/Documents/testing.wav", SampleRate);
             if (duration > SampleRate*30)
             {
                 Console.WriteLine("File written");
@@ -358,160 +282,7 @@ namespace discretefrouiertransform
 
         }
 
-        public short[] readAndWrite(string filePath)
-        {
-            //WaveFileObject file = new WaveFileObject(filePath);
-            // List<short[]> fileBuffers = new List<short[]>();
-            //Console.Write(file.soundData);
-
-            List<short> soundData;
-
-            using (FileStream fs = new FileStream(filePath, FileMode.Open))
-            using (BinaryReader br = new BinaryReader(fs))
-            {
-                try
-                {
-                    br.ReadBytes(4);
-                    br.ReadUInt32();
-                    br.ReadBytes(4);
-                    byte[] temp = br.ReadBytes(4);
-                    string chunk = System.Text.Encoding.UTF8.GetString(temp);
-                    if (chunk != "fmt ")
-                    {
-                        byte[] junk = br.ReadBytes(36);
-                    }
-                    else
-                    {
-
-                    }
-                    br.ReadUInt32();
-                    br.ReadUInt16();
-                    br.ReadUInt16();
-                    br.ReadUInt32();
-                    br.ReadUInt32();
-                    br.ReadUInt16();
-                    br.ReadUInt16();
-                    br.ReadBytes(4);
-                    br.ReadBytes(2);
-                    br.ReadUInt32();
-
-                    soundData = new List<short>();
-
-                    while(br.BaseStream.Position != br.BaseStream.Length)
-                    {
-                        soundData.Add(br.ReadInt16());
-                    }
-
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                    throw;
-                }
-                finally
-                {
-                    if (br != null)
-                    {
-                        br.Close();
-                    }
-                    if (fs != null)
-                    {
-                        fs.Close();
-                    }
-                }
-            }
-
-            short[] audioData =  new short[soundData.Count];
-
-            for (int i = 0; i < soundData.Count; i++)
-                {
-                audioData[i] = soundData[i];
-                }
-            return audioData;
-        }
-
-        public void BeamAndWrite(short[] file1, short[] file2)
-        {
-            splitInput(file1);
-
-            for (int j = 0; j < Buffers.Count; j++)
-            {
-                //    //Console.WriteLine("Buffer: " + Buffers.Count);
-                //    //Console.WriteLine("runs " + j);
-                double AngleList = 45;
-                double LenghtMic = 0.075;
-                ShiftedBuffers[j] = timeDelaySignal(Buffers[j], (1) * LenghtMic * Math.Sin(AngleList * Math.PI / 180) / 343);
-            }
-
-            for (int j = 0; j < OutputSignal.Length; j++)
-            {
-                //double hann = 0.5 * (1 - Math.Cos(2 * Math.PI * j / (ShiftedBuffers[0].Length - 1)));
-
-                if (firsttime != true && j < ShiftedBuffers[0].Length / 2)
-                {
-                    double hann1 = 0.5 * (1 - Math.Cos(2 * Math.PI * (j) / (ShiftedBuffers[0].Length - 1)));
-                    double hann2 = 0.5 * (1 - Math.Cos(2 * Math.PI * (j + ShiftedBuffers[0].Length / 2) / (ShiftedBuffers[0].Length - 1)));
-                    OutputSignal[j] = (hann1 * ShiftedBuffers[0][j]) + (hann2 * ShiftedBuffers[3 + (i - 1) % 2][j + ShiftedBuffers[3 + (i - 1) % 2].Length / 2]);
-                }
-                else if (firsttime == true && j < ShiftedBuffers[0].Length / 2)
-                {
-                    //double hann = 0.5 * (1 - Math.Cos(2 * Math.PI * j / (ShiftedBuffers[0].Length - 1)));
-                    OutputSignal[j] = ShiftedBuffers[0][j];
-                }
-                else if (j >= ShiftedBuffers[0].Length / 2 && j < ShiftedBuffers[0].Length)
-                {
-                    double hann1 = 0.5 * (1 - Math.Cos(2 * Math.PI * j / (ShiftedBuffers[0].Length - 1)));
-                    double hann2 = 0.5 * (1 - Math.Cos(2 * Math.PI * (j - ShiftedBuffers[0].Length / 2) / (ShiftedBuffers[0].Length - 1)));
-                    OutputSignal[j] = (hann1 * ShiftedBuffers[0][j]) + (hann2 * ShiftedBuffers[1][j - ShiftedBuffers[0].Length / 2]);
-                }
-                else if (j >= ShiftedBuffers[0].Length && j < ShiftedBuffers[0].Length + ShiftedBuffers[0].Length / 2)
-                {
-                    double hann1 = 0.5 * (1 - Math.Cos(2 * Math.PI * (j - ShiftedBuffers[0].Length / 2) / (ShiftedBuffers[0].Length - 1)));
-                    double hann2 = 0.5 * (1 - Math.Cos(2 * Math.PI * (j - ShiftedBuffers[0].Length) / (ShiftedBuffers[0].Length - 1)));
-
-
-                    OutputSignal[j] = (hann1 * ShiftedBuffers[1][j - ShiftedBuffers[1].Length / 2]) +
-                                      (hann2 * ShiftedBuffers[2][j - ShiftedBuffers[2].Length]);
-                }
-                else if (j >= ShiftedBuffers[0].Length + ShiftedBuffers[0].Length / 2)
-                {
-                    double hann1 = 0.5 * (1 - Math.Cos(2 * Math.PI * (j - (ShiftedBuffers[0].Length)) / (ShiftedBuffers[0].Length - 1)));
-                    double hann2 = 0.5 * (1 - Math.Cos(2 * Math.PI * (j - (ShiftedBuffers[0].Length + ShiftedBuffers[0].Length / 2)) / (ShiftedBuffers[0].Length - 1)));
-
-                    OutputSignal[j] = (hann1 * ShiftedBuffers[2][j - ShiftedBuffers[2].Length]) +
-                                      (hann2 * ShiftedBuffers[3 + (i % 2)][j - (ShiftedBuffers[0].Length + ShiftedBuffers[0].Length / 2)]);
-                }
-            }
-
-            i++;
-            firsttime = false;
-
-            //printBuffer(mics[0], "ORIGINAL SIGNAL");
-            //printBuffer(OutputSignal, "FINALSHIFTED SIGNAL");
-
-            double[] beamformedSignal = new double[file1.Length];
-            for (int j = 0; j < beamformedSignal.Length; j++)
-                beamformedSignal[j] = (file2[j] + OutputSignal[j]) / 2;
-
-            for (int j = 0; j < beamformedSignal.Length; j++)
-            {
-                byte[] tempByteArr = BitConverter.GetBytes((short)beamformedSignal[j]);
-                //Console.WriteLine("Starting to Append file");
-                writer.Write(tempByteArr, 0, tempByteArr.Length);
-                //Console.WriteLine("File Appended");
-                duration++;
-                //for (int l = 0; l < 2; l++)
-                //{
-                //    byteArr.Add(tempByteArr[l]);
-                //}
-
-            }
-
-
-            writer.Close();
-            Console.Write("Created File!");
-        }
-
+        
         private void splitInput(short[] input)
         {
             for (int index = 0; index < input.Length; index++)
